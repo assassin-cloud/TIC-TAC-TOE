f1a1f7d19254bf3e3a242289b252879f7f11699a
Assassin Spider <raif75219@gmail.com>
2026-09-23 18:28:36 +0500

    Implement main game loop for Tic Tac Toe

---
+++ tic_tac_toe/main.cpp
@@ -0,0 +1,36 @@
+#include "function.h"
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string winner;
+    string anything;
+    while(true){
+        menu();
+        cout << "Input:" << endl;
+        int userinput {};
+        cin >> userinput;
+        if(cin.fail()){
+            cinfail();
+        }
+        else{
+            if(userinput == 1){
+                drawboard();
+                putmarker(winner);
+                resetboard();
+                winner = " ";
+            }
+            else if(userinput == 2){
+                break;
+            }
+            else{
+                cout << "Invalid Input!" << endl;
+                wait(anything);
+                if(anything == "q"){
+                    break;
+                }
+            }
+        }
+    }
+}
