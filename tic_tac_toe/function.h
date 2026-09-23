bf358468ff153a3131af48eb72ea1eb547a405bd
Assassin Spider <raif75219@gmail.com>
2026-09-23 18:29:50 +0500

    Add function declarations for Tic Tac Toe

---
+++ tic_tac_toe/function.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+extern std::string game[3][3];
+
+// Backbone of program flow
+void menu();
+void wait(std::string& anything);
+void cinfail();
+void drawboard();
+bool isdraw();
+void resetboard();
+
+// Essential functions
+bool checkwinner(std::string& winner);
+void putmarker(std::string& winner);
