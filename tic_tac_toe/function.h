8a026424e2e2b00309a5a6ff97e25c0e326876a2
Assassin Spider <raif75219@gmail.com>
2026-09-24 16:33:38 +0500

    Add optionsmenu function declaration

--- tic_tac_toe/function.h
+++ tic_tac_toe/function.h
@@ -5,6 +5,7 @@ extern std::string game[3][3];
 
 // Backbone of program flow
 void menu();
+void optionsmenu();
 void wait(std::string& anything);
 void cinfail();
 void drawboard();
@@ -12,5 +13,6 @@ bool isdraw();
 void resetboard();
 
 // Essential functions
+int takeinputfromuser();
 bool checkwinner(std::string& winner);
 void putmarker(std::string& winner);
